20abe5cba36ba3916365db4a184baa5f501e0b68
Mike Heft <mikeheft@gmail.com>
2024-08-05 06:27:21 -0600

    Add interface and structs for shapes

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type bot interface {
 	getGreeting() string
+}
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
 }
 
 type englishBot struct{}
@@ -20,10 +36,22 @@ func printGreeting(b bot) {
 	fmt.Println(b.getGreeting())
 }
 
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
 func (englishBot) getGreeting() string {
 	return "Hello There!"
 }
 
 func (spanishBot) getGreeting() string {
 	return "Hola!"
+}
+
+func (s square) getArea() float64 {
+	return math.Pow(s.sideLength, 2)
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
 }
